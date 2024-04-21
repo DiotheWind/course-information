5fb55655b8a8691f5e530e832116201b6df11d98
Deo Seth <diothewind@gmail.com>
2024-04-21 18:21:24 +0800

    Add Content component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,14 @@ const Header = (props) => {
   )
 }
 
+const Content = (props) => {
+  return (
+    <>
+      <p>{props.part} {props.exercises}</p>
+    </>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -29,6 +37,9 @@ const App = () => {
       </p>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> */}
       <Header heading={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
     </div>
   )
 }
